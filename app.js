fdd55ce9821407319419ddaf355e35dc1afd5014
danishyaqoobofficial <danishyaqoob117@gmail.com>
2024-07-24 17:34:35 +0500

    last commit

--- app.js
+++ app.js
@@ -121,6 +121,22 @@ let input_btn = document.getElementById('input-btn');
 let inputNumber = document.getElementById('number');
 let Error = document.getElementById('error');
 
+inputNumber.addEventListener('keyup', (e) =>{
+    if (e.keyCode === 13) {
+        if (inputNumber.value === '') {
+            inputNumber.classList.add('!border-[#FF0000]');
+        }else if (inputNumber.value > 360) {
+            Error.innerHTML = 'Your value is too long'
+        }else if (inputNumber.value === '' || inputNumber.value < 361) {
+            rotate_box.classList.add(`rotate-[${inputNumber.value/2}deg]`);
+            inputNumber.classList.remove('!border-[#FF0000]');
+            Error.innerHTML = ''
+        }
+    }else{
+
+    }
+})
+
 
 input_btn.addEventListener('click' , () =>{
     if (inputNumber.value === '') {
