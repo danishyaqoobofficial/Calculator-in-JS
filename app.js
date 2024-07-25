a601daf5fe0a6e7c5fe57d13bc89536c5048a813
danishyaqoobofficial <danishyaqoob117@gmail.com>
2024-07-25 11:28:34 +0500

    initial commit

--- app.js
+++ app.js
@@ -120,6 +120,9 @@ let rotate_box = document.getElementById('rotate_box');
 let input_btn = document.getElementById('input-btn');
 let inputNumber = document.getElementById('number');
 let Error = document.getElementById('error');
+let one= document.getElementById('one');
+let two = document.getElementById('two');
+let three = document.getElementById('three');
 
 inputNumber.addEventListener('keyup', (e) =>{
     if (e.keyCode === 13) {
@@ -127,25 +130,46 @@ inputNumber.addEventListener('keyup', (e) =>{
             inputNumber.classList.add('!border-[#FF0000]');
         }else if (inputNumber.value > 360) {
             Error.innerHTML = 'Your value is too long'
-        }else if (inputNumber.value === '' || inputNumber.value < 361) {
+        }else if (inputNumber.value !== '' && inputNumber.value < 361) {
             rotate_box.classList.add(`rotate-[${inputNumber.value/2}deg]`);
             inputNumber.classList.remove('!border-[#FF0000]');
             Error.innerHTML = ''
+            if (inputNumber.value <= 120 && inputNumber.value > 0 ) {
+                one.classList.add('!text-[#000]')
+            }else if (inputNumber.value <= 240 && inputNumber.value > 121 ) {
+                two.classList.add('!text-[#000]')
+            }else if (inputNumber.value <= 360 && inputNumber.value > 241 ) {
+                three.classList.add('!text-[#000]')
+            }else{
+                one.classList.remove('!text-[#000]')
+                two.classList.remove('!text-[#000]')
+                three.classList.remove('!text-[#000]')
+            }
         }
     }else{
 
     }
 })
 
-
 input_btn.addEventListener('click' , () =>{
     if (inputNumber.value === '') {
         inputNumber.classList.add('!border-[#FF0000]');
     }else if (inputNumber.value > 360) {
         Error.innerHTML = 'Your value is too long'
-    }else if (inputNumber.value === '' || inputNumber.value < 361) {
+    }else if (inputNumber.value !== '' && inputNumber.value < 361) {
         rotate_box.classList.add(`rotate-[${inputNumber.value/2}deg]`);
         inputNumber.classList.remove('!border-[#FF0000]');
         Error.innerHTML = ''
+        if (inputNumber.value <= 120 && inputNumber.value > 0 ) {
+            one.classList.add('!text-[#000]')
+        }else if (inputNumber.value <= 240 && inputNumber.value > 121 ) {
+            two.classList.add('!text-[#000]')
+        }else if (inputNumber.value <= 360 && inputNumber.value > 241 ) {
+            three.classList.add('!text-[#000]')
+        }else{
+            one.classList.remove('!text-[#000]')
+            two.classList.remove('!text-[#000]')
+            three.classList.remove('!text-[#000]')
+        }
     }
 })
