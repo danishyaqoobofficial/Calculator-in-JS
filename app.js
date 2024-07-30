993a6e6de00a9b5094b6e44101c4ed6854a60db1
danishyaqoobofficial <danishyaqoob117@gmail.com>
2024-07-30 17:57:15 +0500

    initial commit

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
+let loader = document.getElementById('loader');
+
+window.addEventListener('load', async () => {
+    loader.classList.add('!block');
+    try {
+        const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const data = await res.json();
+        data.map((post) => {
+            let main_post_data = document.getElementById('main_post_data');
+            let newElement = document.createElement('div');
+            newElement.id = `post-${post.id}`;
+            let element = `<div class="flex items-center gap-[30px] px-[22px] py-[11px] border-t">
+                <p class="text-[14px] text-[#000]/40">${post.id}</p>
+                <p class="flex-1 text-center line-clamp-1 text-[14px] text-[#000]/40">${post.title}</p>
+            </div>`;
+            newElement.innerHTML = element;
+            main_post_data.appendChild(newElement);
+        });
+    } catch (error) {
+        console.log('error', error);
+    } finally {
+        loader.classList.remove('!block');
+    }
+});
+
+
+
 let delete_btn = document.getElementById('delete_btn');
 let delete_input = document.getElementById('delete_input');
 
@@ -25,27 +52,28 @@ delete_btn.addEventListener('click', () => {
     }
 });
 
-let loader = document.getElementById('loader');
 
-window.addEventListener('load', async () => {
-    loader.classList.add('!block');
-    try {
-        const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-        const data = await res.json();
-        data.map((post) => {
-            let main_post_data = document.getElementById('main_post_data');
-            let newElement = document.createElement('div');
-            newElement.id = `post-${post.id}`;
-            let element = `<div class="flex items-center gap-[30px] px-[22px] py-[11px] border-t">
-                <p class="text-[14px] text-[#000]/40">${post.id}</p>
-                <p class="flex-1 text-center line-clamp-1 text-[14px] text-[#000]/40">${post.title}</p>
-            </div>`;
-            newElement.innerHTML = element;
-            main_post_data.appendChild(newElement);
-        });
-    } catch (error) {
-        console.log('error', error);
-    } finally {
-        loader.classList.remove('!block');
+
+
+let user_input = document.getElementById('user_input');
+let user_btn = document.getElementById('user_btn');
+
+user_btn.addEventListener('click', () =>{
+    if (user_input.value === '') {
+        user_input.classList.add('border-red-300');
+
+    } else{
+        user_input.classList.remove('border-red-300');
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+            title: user_input.value,
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+        })
+        .then((response) => response.json())
+        .then((json) => console.log(json));
     }
-});
+})
